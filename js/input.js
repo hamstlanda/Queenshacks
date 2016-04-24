257416e9934f2d0e2602efb6a116c971db490108
hamslanda <hamstlanda@gmail.com>
2016-04-24 10:45:16 -0400

    link to next page works, saving input does not

--- js/input.js
+++ js/input.js
@@ -1,11 +1,17 @@
 function finalRes(form) {
     'use strict';
+    document.getElementById("people").innerHTML = window.location.search;
     var peopleIs = form.numPeople.value;
     var widthIs = form.width.value;
     var radiusIs = form.radius.value; 
+   
     var angle= (180/peopleIs);
     var sinValue = Math.sin(angle/180*Math.PI)
     var sizeOfCut = (2 * radiusIs * sinValue);
-    alert("size of cut is " +sizeOfCut );
+    //alert("size of cut is " +sizeOfCut );
+    
 }
-
+function cutSizePage(form){
+    'use strict';
+     window.open("file:///C:/Users/Me/Desktop/Queenshacks/cutSize.html");
+}
