5be5c6c0af3c80d88363b0d3d084b3f30538734e
hamslanda <hamstlanda@gmail.com>
2016-04-23 22:56:30 -0400

    put working js into separate js file

--- js/input.js
+++ js/input.js
@@ -1,4 +1,12 @@
-var input = document.getElementById("radius");
-function showInput(form) {
-document.getElementById(Output).innerHTML = 6;   
-}
+ function peopleRes (form) {
+                var peopleIs = form.numPeople.value;
+                alert ("Number of people you entered is: " + peopleIs);
+            }
+            function widthRes(form){
+                var widthIs = form.width.value;
+                alert("The width is: "+widthIs);
+            }
+            function radiusRes(form){
+                var radiusIs = form.radius.value;
+                alert("The radius is: "+radiusIs);
+            }
