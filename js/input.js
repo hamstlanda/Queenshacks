2c73f96ccd666cbaa44d11d7d899a9f012a7bf10
hamslanda <hamstlanda@gmail.com>
2016-04-24 09:42:04 -0400

    alert gives the correct cut size

--- js/input.js
+++ js/input.js
@@ -1,12 +1,11 @@
- function peopleRes (form) {
-                var peopleIs = form.numPeople.value;
-                alert ("Number of people you entered is: " + peopleIs);
-            }
-            function widthRes(form){
-                var widthIs = form.width.value;
-                alert("The width is: "+widthIs);
-            }
-            function radiusRes(form){
-                var radiusIs = form.radius.value;
-                alert("The radius is: "+radiusIs);
-            }
+function finalRes(form) {
+    'use strict';
+    var peopleIs = form.numPeople.value;
+    var widthIs = form.width.value;
+    var radiusIs = form.radius.value; 
+    var angle= (180/peopleIs);
+    var sinValue = Math.sin(angle/180*Math.PI)
+    var sizeOfCut = (2 * radiusIs * sinValue);
+    alert("size of cut is " +sizeOfCut );
+}
+
